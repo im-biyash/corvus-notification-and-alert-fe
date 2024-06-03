67cc273395d54fb473357b8285c595e9d71f5014
biyash <bsstha21@gmail.com>
2024-06-03 22:43:24 +0545

    setup next shadcn n navbar

---
+++ app/Mycomponents/Navbar.tsx
@@ -0,0 +1,21 @@
+"use client"
+import React from 'react'
+const Navbar = () => {
+  return (
+   
+    <nav className='flex justify-between items-center p-2 '>
+      <div className="logo">
+          <img src = "" className ="h-12 w-auto" alt="" />
+      </div>
+      <ul className='flex gap-3 p-2 font-semibold'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>  
+        <li>Blog</li>
+      </ul>
+     
+`    </nav>
+  )
+}
+
+export default Navbar
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Navbar from "./Mycomponents/Navbar";
+export default function Home() {
+  return (
+    <main className="min-h-screen ">
+      <Navbar/>
+      <h1>hero</h1>
+    </main>
+  );
+}
