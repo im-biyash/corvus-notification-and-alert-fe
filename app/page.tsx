6f79a232b11838de618374e492e229c525bbced4
biyash <bsstha21@gmail.com>
2024-06-06 12:16:29 +0545

    change

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import  Sidebar  from './Mycomponents/Sidebar';
-import Hero from './Mycomponents/Hero';
+import Sidebar from "../components/Sidebar";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
     <main className=" home min-h-screen ">
-      <div className='flex items-center justify-between'>
-      <Sidebar/>
-      <Hero/>
+      <div className="flex items-center justify-between">
+        <Sidebar />
+        <Hero />
       </div>
     </main>
   );
